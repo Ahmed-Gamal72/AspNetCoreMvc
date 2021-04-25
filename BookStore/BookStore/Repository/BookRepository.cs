77ab55e3f11a760b4430d1d40267d3cbc7a97a0c
Ahmed-Gamal72 <ahmdgma.97@gmail.com>
2021-04-26 01:13:25 +0200

    Add Libman To Use ClientSide Library

---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,44 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+
+        }
+
+        public List<BookModel> SearchBook(string title , string AuthorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(AuthorName)).ToList();
+
+        }
+
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel (){Id=1, Title="MVC", Author="Ahmed"},
+                new BookModel (){Id=2, Title=".Net Core", Author="Ahmed"},
+                new BookModel (){Id=3, Title="C#", Author="Mahmoud"},
+                new BookModel (){Id=4, Title="Java", Author="mohamed"},
+                new BookModel (){Id=5, Title="PHP", Author="mohamed"}
+
+            };
+        }
+
+
+    }
+}
